0bd61bf5c84295d6fd09a58c8874656fd35cc8cf
deweyvm <deweyvm@gmail.com>
2014-04-08 20:13:05 -0400

    added state machine

---
+++ src/Dungeon/Data/Array2d.hs
@@ -0,0 +1,50 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+module Dungeon.Data.Array2d where
+
+
+import Data.Maybe
+import qualified Data.Vector as Vec
+
+data Array2d elt = Array2d Int Int (Vec.Vector elt)
+
+instance Functor Array2d where
+    fmap f (Array2d cols rows v) = Array2d cols rows $ Vec.map f v
+
+
+select :: a -> a -> Bool -> a
+select f t p = if p then t else f
+
+unsafeGet :: Array2d a -> Int -> a
+unsafeGet (Array2d _ _ v) k = v Vec.! k
+
+get :: Array2d a -> Int -> Int -> Maybe a
+get arr@(Array2d cols rows _) i j =
+    select (Just $ unsafeGet arr $ fromCoords arr i j)
+           Nothing
+           (i < 0 || i > cols - 1 || j < 0 || j > rows - 1)
+
+getOrElse :: Array2d a -> a -> Int -> Int -> a
+getOrElse arr e x y = fromMaybe e (get arr x y)
+
+toCoords :: Array2d a -> Int -> (Int, Int)
+toCoords (Array2d cols _ _) k = (k `mod` cols, k `quot` cols)
+
+fromCoords :: Array2d a -> Int -> Int -> Int
+fromCoords (Array2d cols _ _) i j = i + j* cols
+
+imap :: (Int -> Int -> a -> b) -> Array2d a -> Array2d b
+imap f arr@(Array2d cols rows v) = Array2d cols rows $ mapped
+    where zipped = Vec.zip (Vec.fromList [0..(cols*rows)-1]) v
+          mapped = Vec.map (\(k, p) -> let (x, y) = toCoords arr k in f x y p) zipped
+
+
+zipWithIndex :: Array2d a -> Array2d (Int, Int, a)
+zipWithIndex arr = imap (,,) arr
+
+
+tabulate :: Int -> Int -> a -> (Int -> Int -> a) -> Array2d a
+tabulate cols rows initial f =
+    imap (\x y _ -> f x y) base
+    where base = Array2d cols rows $ Vec.replicate (cols*rows) initial
+
+
--- src/Main.hs
+++ src/Main.hs
@@ -1,52 +1,21 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 
+import Prelude hiding (concat, foldl)
 import Codec.Picture
-import qualified Data.Vector as Vec
-import Data.Maybe
-import Data.Function
 import System.Random
+import Data.Function
 import Data.Word
-data Array2d elt = Array2d Int Int (Vec.Vector elt)
-
-select :: a -> a -> Bool -> a
-select f t p = if p then t else f
-
-unsafeGet :: Array2d a -> Int -> a
-unsafeGet (Array2d _ _ v) k = v Vec.! k
-
-get :: Array2d a -> Int -> Int -> Maybe a
-get arr@(Array2d cols rows _) i j =
-    select (Just $ unsafeGet arr $ fromCoords arr i j)
-           Nothing
-           (i < 0 || i > cols - 1 || j < 0 || j > rows - 1)
-
-toCoords :: Array2d a -> Int -> (Int, Int)
-toCoords (Array2d cols _ _) k = (k `mod` cols, k `quot` cols)
-
-fromCoords :: Array2d a -> Int -> Int -> Int
-fromCoords (Array2d cols _ _) i j = i + j* cols
-
-aimap :: (Int -> Int -> a -> b) -> Array2d a -> Array2d b
-aimap f arr@(Array2d cols rows v) = Array2d cols rows $ mapped
-    where zipped = Vec.zip (Vec.fromList [0..(cols*rows)-1]) v
-          mapped = Vec.map (\(k, p) -> let (x, y) = toCoords arr k in f x y p) zipped
-
-amap :: (a -> b) -> Array2d a -> Array2d b
-amap f arr@(Array2d cols rows v) = Array2d cols rows $ Vec.map f v
-
-zipWithIndex :: Array2d a -> Array2d (Int, Int, a)
-zipWithIndex arr = aimap (,,) arr
-
-
-tabulate :: Int -> Int -> a -> (Int -> Int -> a) -> Array2d a
-tabulate cols rows initial f =
-    aimap (\x y _ -> f x y) base
-    where base = Array2d cols rows $ Vec.replicate (cols*rows) initial
+import Data.Maybe
+import Data.Foldable
+import Control.Monad
+import Debug.Trace
+import qualified Data.Vector as Vec
+import Dungeon.Data.Array2d
 
 saveMap :: FilePath -> Array2d PixelRGBA8 -> IO ()
 saveMap path arr@(Array2d cols rows _) =
     writePng path img
-    where img = generateImage (\x y -> fromMaybe (PixelRGBA8 0 0 0 255) (get arr x y)) cols rows
+    where img = generateImage (getOrElse arr (PixelRGBA8 0 0 0 255) ) cols rows
 
 divf :: Int -> Int -> Float
 divf = (/) `on` fromIntegral
@@ -65,12 +34,46 @@ makeRandom cols rows =
 bool2Pixel :: Bool -> PixelRGBA8
 bool2Pixel = select (PixelRGBA8 0 0 0 255) (PixelRGBA8 0 128 0 255)
 
+myTrace :: Show a => a -> a
+myTrace x = traceShow x x
+
+getOccupants :: Array2d a -> Int -> Int -> [a]
+getOccupants arr i j =
+    catMaybes $ map (uncurry (get arr)) [ (i, j)
+                                        , (i, (j - 1))
+                                        , (i, (j + 1))
+                                        , ((i - 1), j)
+                                        , ((i + 1), j)
+                                        , ((i - 1), (j - 1))
+                                        , ((i + 1), (j + 1))
+                                        , ((i - 1), (j + 1))
+                                        , ((i + 1), (j - 1))
+                                        ]
+
+
+(.:) :: (c -> d) -> (a -> b -> c) -> (a -> b -> d)
+(.:) = ((.) . (.))
+
+(.::) :: (d -> e) -> (a -> b -> c -> d) -> (a -> b -> c -> e)
+(.::) = (.:) . (.)
+
+count ::  (a -> Bool) -> [a] -> Int
+count f = length . (filter f)
+
+countOccupants :: (a -> Bool) -> Array2d a -> Int -> Int -> Int
+countOccupants f = (count f) .:: (getOccupants)
+
+runMachine :: Array2d Bool -> Array2d Bool
+runMachine arr = imap (\i j _ -> (countOccupants id arr i j) >= 5) arr
+
 main :: IO ()
 main = do
-    let cols = 500
-    let rows = 500
+    let cols = 200
+    let rows = 200
     let img = tabulate cols rows (PixelRGBA8 0 0 255 255) $ pixToColor cols rows
-    let img2 = amap bool2Pixel $ makeRandom cols rows
+    let initial = makeRandom cols rows
+    let permuted = iterate runMachine initial !! 4
+    let img2 = fmap bool2Pixel permuted
 
     saveMap "test.png" img2
     putStrLn "this is a test"
