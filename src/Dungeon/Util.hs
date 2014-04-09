7dd956885b1125f3d4c5e76a141bec4ac8ddf7a2
deweyvm <deweyvm@gmail.com>
2014-04-08 20:30:51 -0400

    moved util functions to separate module

---
+++ src/Dungeon/Util.hs
@@ -0,0 +1,20 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+module Dungeon.Util where
+
+import Debug.Trace
+import Data.Function
+
+myTrace :: Show a => a -> a
+myTrace x = traceShow x x
+
+divf :: Int -> Int -> Float
+divf = (/) `on` fromIntegral
+
+(.:) :: (c -> d) -> (a -> b -> c) -> (a -> b -> d)
+(.:) = ((.) . (.))
+
+(.::) :: (d -> e) -> (a -> b -> c -> d) -> (a -> b -> c -> e)
+(.::) = (.:) . (.)
+
+count ::  (a -> Bool) -> [a] -> Int
+count f = length . (filter f)
--- src/Main.hs
+++ src/Main.hs
@@ -3,13 +3,10 @@
 import Prelude hiding (concat, foldl)
 import Codec.Picture
 import System.Random
-import Data.Function
 import Data.Word
 import Data.Maybe
-import Data.Foldable
-import Control.Monad
-import Debug.Trace
 import qualified Data.Vector as Vec
+import Dungeon.Util
 import Dungeon.Data.Array2d
 
 saveMap :: FilePath -> Array2d PixelRGBA8 -> IO ()
@@ -17,8 +14,7 @@ saveMap path arr@(Array2d cols rows _) =
     writePng path img
     where img = generateImage (getOrElse arr (PixelRGBA8 0 0 0 255) ) cols rows
 
-divf :: Int -> Int -> Float
-divf = (/) `on` fromIntegral
+
 
 pixToColor :: Int -> Int -> Int -> Int -> PixelRGBA8
 pixToColor cols rows x y =
@@ -34,9 +30,6 @@ makeRandom cols rows =
 bool2Pixel :: Bool -> PixelRGBA8
 bool2Pixel = select (PixelRGBA8 0 0 0 255) (PixelRGBA8 0 128 0 255)
 
-myTrace :: Show a => a -> a
-myTrace x = traceShow x x
-
 getOccupants :: Array2d a -> Int -> Int -> [a]
 getOccupants arr i j =
     catMaybes $ map (uncurry (get arr)) [ (i, j)
@@ -51,15 +44,6 @@ getOccupants arr i j =
                                         ]
 
 
-(.:) :: (c -> d) -> (a -> b -> c) -> (a -> b -> d)
-(.:) = ((.) . (.))
-
-(.::) :: (d -> e) -> (a -> b -> c -> d) -> (a -> b -> c -> e)
-(.::) = (.:) . (.)
-
-count ::  (a -> Bool) -> [a] -> Int
-count f = length . (filter f)
-
 countOccupants :: (a -> Bool) -> Array2d a -> Int -> Int -> Int
 countOccupants f = (count f) .:: (getOccupants)
 
@@ -70,10 +54,8 @@ main :: IO ()
 main = do
     let cols = 200
     let rows = 200
-    let img = tabulate cols rows (PixelRGBA8 0 0 255 255) $ pixToColor cols rows
     let initial = makeRandom cols rows
     let permuted = iterate runMachine initial !! 4
     let img2 = fmap bool2Pixel permuted
 
     saveMap "test.png" img2
-    putStrLn "this is a test"
